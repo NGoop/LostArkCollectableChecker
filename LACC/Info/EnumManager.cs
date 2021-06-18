aded828db5eec826b717996c78e22384e7a4d4c6
silvlightwolf <silvlightwolf@naver.com>
2021-06-19 03:46:09 +0900

    EnumManager 추가

---
+++ LACC/Info/EnumManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+
+namespace LACC.Info
+{
+    // 해당 Attribute 구조는 https://phiru.tistory.com/249 를 참고했습니다.
+
+    interface CollectData
+    {
+        string getValue();
+    }
+
+    public class ContentName : Attribute, CollectData
+    {
+        private string _value;
+
+        public ContentName (string value)
+        {
+            _value = value;
+        }
+
+        public string getValue()
+        {
+            return _value;
+        }
+    }
+
+
+    // Usage Example : EnumManager.GetContent(MindOfIsland.Libeheim)
+
+    public static class EnumManager
+    {
+        private static string GetEnumValue(Type targetType, Object value)
+        {
+            Type type = value.GetType();
+
+            FieldInfo fi = type.GetField(value.ToString());
+            Attribute[] attrs = fi.GetCustomAttributes(targetType, false) as Attribute[];
+
+            return (attrs.Length > 0) ? (attrs[0] as CollectData).getValue() : null;
+        }
+
+        public static string GetContent(Object value)
+        {
+            return GetEnumValue(typeof(ContentName), value);
+        }
+
+    }
+}
---
+++ LACC/Info/MindOfIsland.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LACC.Info
+{
+    // 영어 명칭은 정식 명칭이 아니라, 한국이름을 대충 발음대로 적은것
+    enum MindOfIsland
+    {
+        [ContentName("Btn_MOI_Libeheim")]
+        Libeheim
+    }
+}
